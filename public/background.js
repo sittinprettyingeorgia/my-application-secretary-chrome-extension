61ca5c38711e5d2bc6f322fff586d21936e15ab8
sittinprettyingeorgia <mitchellblake29@aol.com>
2022-11-10 16:39:24 -0800

    .

--- public/background.js
+++ public/background.js
@@ -4,7 +4,25 @@ chrome.runtime.onInstalled.addListener(() => {
   return;
 });
 
-/*
+/**
+ * <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Background</title>
+</head>
+<body>
+    <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-firestore.js"></script>
+    <script src="./scriptmain.bundle.js"></script>
+    <script src="./background.js"></script>
+</body>
+</html>
+ */
+/* EXAMPLE
 var tabid = "";
 let indeedTabId;
 let maintabid;
